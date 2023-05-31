66ba18338d4cbac9f90eb5f17ab559dda20c2710
Matheus Ferreira <matheus.fer33@gmail.com>
2023-05-30 21:21:28 -0300

    feat: front end basic structure and components created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,24 @@
+import Form from "./components/Form";
+import Posts from "./components/Posts";
+
 function App() {
-  return <>teste</>;
+  return (
+    <div className="container mx-auto">
+      <header className="flex justify-center items-center my-3 ">
+        <h1 className="text-3xl font-bold">Places...</h1>
+      </header>
+      <div className="mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+          <div className="col-span-1 sm:col-span-2">
+            <Posts />
+          </div>
+          <div className="col-span-1">
+            <Form />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
